e34cc1f7bd46c76dcd0f7a6844e31478c83644a9
Alexdemanuel <alejandrodemol@gmail.com>
2019-02-07 10:16:02 +0100

    arregalado ejercicio8

--- 04-arrays/Ejercicio-08/index.php
+++ 04-arrays/Ejercicio-08/index.php
@@ -1,85 +1,51 @@
 <?php
 
-echo "<h2>Escribe un script PHP que sobre un array de temperaturas realice las siguientes operaciones:</h2>
-
-<ul>Calcular la media.</ul>
-<ul>Calcular el valor máximo.</ul>
-<ul>Calcular el valor mínimo.</ul>
-<ul>Mostrar todos los valores calculados.</ul>
-El array de temperaturas lo vamos a generar con números aleatorios. El array será de 10 elementos y los valores aletorios generados estarán entre 1 y 30.";
-//Declaro el array
-$valores = array();
+$numeros = array();
 
 
-//inicializo el array con 10 numero aleatorios
+//inicializo el array
 
-$i=0;
-    do{
-        $valores[$i] = rand(1,30);
-        echo "</br>";
-        $i++;
-    }
-while ( $i <= 9);
-
-
-$i=0;
-    do {
-        echo "el Valor de $i es: ".$valores[$i];
-        echo "</br>";
-        $i++;
-    }
- while ($i<10);
-
-
- // Calcular valor medio
-
-$suma = 0;
-$i=0;
-    do {
-        $suma = $suma + $valores[$i];
-        $i++;
-    }
-while($i < 10 );
-
-    $media = $suma / count($valores);
-    echo "El valor medio es: ".$media;
+for ($i=0; $i <= 9; $i++) {
+    $numeros[$i] = rand(1,30);
     echo "</br>";
+    echo $numeros[$i];
+}
+
+$numero_de_elementos = count($numeros);
+echo "<table border='1'>";
+echo "<tr>";
+
+$i=0;
+for ($i=0;$i< $numero_de_elementos;$i++){
+    echo "<td>$i</td>";
+   echo "<br>";
+}
+echo "</tr>";
+
+//ordenar numero de menor a mayor
+sort($numeros);
+echo "<tr>";
+echo "<td> de menor a mayor";
+$i=0;
+for($i=0;$i< $numero_de_elementos;$i++){
+    echo "<td>".$numeros[$i]."</td>";
+   echo "<br>";
+}
+echo "</tr>";
 
 
+echo "<tr>";
+//ordenar numero de menor a mayor
+rsort($numeros);
+echo "<tr>";
+echo "<td> de mayor a menor";
+$i=0;
+for($i=0;$i< $numero_de_elementos;$i++){
+    echo "<td>".$numeros[$i]."</td>";
+   echo "<br>";
+}
+echo "</tr>";
+echo "</table>";
 
-// Valor maximo 
-
-$maximo = -1;
-$i = 0;
-    do{
-        if($valores[$i] > $maximo){
-            $maximo = $valores[$i];
-            $posicion = $i;
-            
-        }
-        $i++;
-    }
-while ($i <10);
-
-echo "El número máximo es: ".$maximo. " y esta en la posición: ".$posicion;
-echo "</br>";
-
-// Valor minimo
-
-
-$minimo = 99;
-$i = 0;
-    do{
-        if($valores[$i] < $minimo){
-            $minimo = $valores[$i];
-            $posicion = $i;
-            
-        }
-        $i++;
-    }
-while ($i <10);
-
-echo "El número mínimo es: ".$minimo. " y esta en la posición: ".$posicion;
-echo "</br>";
 
 ?>
